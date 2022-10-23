94991e3f10652627ebd72addafa9abf762f52264
Andrew Alizaga <aalizaga@vozy.co>
2022-10-22 22:20:13 -0600

    functional test date -> speech working

--- pkg/date.go
+++ pkg/date.go
@@ -1,6 +1,8 @@
 package pkg
 
 import (
+	"time"
+
 	datev1 "github.com/andrewAlizaga/golang_simple_type_convertions/internal/date"
 )
 
@@ -10,3 +12,30 @@ func GetDateLayoutDateAndFormat(dateString string) (layouts []datev1.DateTimeLay
 	layouts, err = datev1.GetDateLayout(dateString)
 	return
 }
+
+
+//Access point for lib consumption
+//case 2, date to redable format
+func DateParseSpeech(date time.Time, language datev1.Language) (speech string, err error){
+	speech, err = datev1.GetSpeech(date, language)
+	return
+}
+
+/*
+
+// LATAM DAYS ARRAY
+var latamDays = [...]string{"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"}
+
+// USA DAYS ARRAY
+var usaDays = [...]string{"Monday", "Thuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+
+// LATAM DAYS ARRAY
+var latamMonths = [...]string{"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+	"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"}
+
+// USA Months ARRAY
+var usaMonths = [...]string{"January", "February", "March", "April", "May", "June", "July",
+	"Agust", "September", "October", "November", "December"}
+
+
+*/
--- test/functional/date_test.go
+++ test/functional/date_test.go
@@ -3,6 +3,7 @@ package functional_test
 import (
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/andrewAlizaga/golang_simple_type_convertions/internal/date"
 )
@@ -15,7 +16,7 @@ func TestCaseGetDateLayout(t *testing.T) {
 	if err != nil {
 		t.Logf(err.Error())
 		t.Fail()
-	}else {
+	} else {
 		t.Log("CONVERTION RESULTS: ")
 		t.Log(result)
 
@@ -23,8 +24,8 @@ func TestCaseGetDateLayout(t *testing.T) {
 			err = errors.New("EMPTY ARRAY, NO ABLE TO FIND DATE STRING MATCHES, CHECK THE STRING IS COMPATIBLE O NOT")
 			t.Logf(err.Error())
 			t.Fail()
-		}else {
-			for idx, lay := range(result) {
+		} else {
+			for idx, lay := range result {
 				t.Log(idx)
 				t.Log("layout: ", lay)
 				t.Log("format: ", lay.Format())
@@ -32,4 +33,50 @@ func TestCaseGetDateLayout(t *testing.T) {
 		}
 	}
 
+}
+
+func TestDateToSpeech(t *testing.T) {
+	t.Log("INVOKE TEST - TestDateToSpeech")
+
+	dateTime := time.Now()
+
+	t.Log("TESTING DATE: ")
+	t.Log(dateTime)
+
+	speech, err := date.GetSpeech(dateTime, date.LANGUAGE_SPANISH)
+
+	if err == nil {
+		t.Log("GOT SPEECH: ", speech)
+	}else {
+		t.Fatalf(err.Error())
+		return
+	}
+
+	//call controller
+}
+
+func TestGetDateStringToSpeech(t *testing.T) {
+	t.Log("INVOKE TEST - TestCaseGetDateLayout")
+
+	result, err := date.GetDateLayout("Mon")
+
+	if err != nil {
+		t.Logf(err.Error())
+		t.Fail()
+	} else {
+		t.Log("CONVERTION RESULTS: ")
+		t.Log(result)
+
+		if len(result) == 0 {
+			err = errors.New("EMPTY ARRAY, NO ABLE TO FIND DATE STRING MATCHES, CHECK THE STRING IS COMPATIBLE O NOT")
+			t.Logf(err.Error())
+			t.Fail()
+		} else {
+			for idx, lay := range result {
+				t.Log(idx)
+				t.Log("layout: ", lay)
+				t.Log("format: ", lay.Format())
+			}
+		}
+	}
 }
